b910716d46369b2e6d9e0ffb5ccd0548df764a02
Kamil Gorny <kamil.gorny@nsure.com>
2022-01-28 09:50:22 +0100

    Add Keyvault secret names to terraform

--- FrontendApi/dev/infrastructure/keyvault.tf
+++ FrontendApi/dev/infrastructure/keyvault.tf
@@ -7,5 +7,38 @@ resource "azurerm_key_vault" "nsure-dev-keyvault" {
   soft_delete_retention_days = "10"
   sku_name = "standard"
   tenant_id = data.azurerm_client_config.current.tenant_id
+  access_policy {
+    object_id = azurerm_app_service.nsure-dev.id
+    tenant_id = data.azurerm_client_config.current.tenant_id
+
+    key_permissions = [
+      "Get",
+      "List"
+    ]
+
+    secret_permissions = [
+      "UnwrapKey",
+      "WrapKey"
+    ]
+  }
   
-} 
+  
+}
+
+resource "azurerm_key_vault_secret" "prod-nsure-keyvault-cosmosdb-connection-string" {
+  name         = "CosmosDb--ConnectionString"
+  value        = ""
+  key_vault_id = azurerm_key_vault.nsure-dev-keyvault.id
+}
+
+resource "azurerm_key_vault_secret" "prod-nsure-keyvault-frontendcosmosdb-connection-string" {
+  name         = "FrontendCosmosDb--ConnectionString"
+  value        = ""
+  key_vault_id = azurerm_key_vault.nsure-dev-keyvault.id
+}
+
+resource "azurerm_key_vault_secret" "prod-nsure-keyvault-jwt-secret-key" {
+  name         = "JWT--SecretKey"
+  value        = ""
+  key_vault_id = azurerm_key_vault.nsure-dev-keyvault.id
+}
