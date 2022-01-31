907642aab81757a9f75a863755fc7a025f91bcfa
kamil-gorny <kamil.gorny99@gmail.com>
2022-01-31 16:55:39 +0100

    Add Keyvault secrets and access policies

--- Backoffice/infrastructure/keyvault.tf
+++ Backoffice/infrastructure/keyvault.tf
@@ -8,4 +8,19 @@ resource "azurerm_key_vault" "nsure-dev-backoffice-keyvault" {
   sku_name = "standard"
   tenant_id = data.azurerm_client_config.current.tenant_id
 
-} 
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = azurerm_app_service.nsure-dev.id
+    
+    secret_permissions = [
+      "list",
+      "get",
+    ]
+  }
+
+}
+resource "azurerm_key_vault_secret" "example" {
+  name         = "SqlDatabase--ConnectionString"
+  value        = ""
+  key_vault_id = azurerm_key_vault.nsure-dev-backoffice-keyvault.id
+}
