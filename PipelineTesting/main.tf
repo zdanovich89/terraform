3bf1ada56626628e497ea58699908908c64a6b00
Yuahen Zdanovich <yauhen.zdanovich@nsure.com>
2023-05-17 16:41:59 +0200

    fix: change main.tf

--- PipelineTesting/main.tf
+++ PipelineTesting/main.tf
@@ -7,3 +7,9 @@ resource "azurerm_resource_group" "rg-terraform-dev-01" {
   name     = "rg-terraform-dev-01"
   location = "East US"
 }
+resource "azurerm_management_lock" "resource-group-level" {
+  name       = "resource-group-level"
+  scope      = azurerm_resource_group.rg-terraform-dev-01.id
+  lock_level = "ReadOnly"
+  notes      = "This Resource Group is Read-Only"
+}
