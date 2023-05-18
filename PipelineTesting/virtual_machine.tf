f640e2cd1c147515f1782c0dce825668be6c2746
Yuahen Zdanovich <yauhen.zdanovich@nsure.com>
2023-05-18 11:32:34 +0200

    modified:   PipelineTesting/virtual_machine.tf

--- PipelineTesting/virtual_machine.tf
+++ PipelineTesting/virtual_machine.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+    features {}
+    alias           = "core_network"
+    subscription_id = "00000000-0000-0000-0000-000000000000"
+    client_id       = "00000000-0000-0000-0000-000000000000"
+    client_secret   = "S3cR3t20!"
+    tenant_id       = "00000000-0000-0000-0000-000000000000"
+}
 resource "azurerm_resource_group" "rg-terraform-dev-01" {
     name     = "rg-terraform-dev-01"
     location = "East US"
