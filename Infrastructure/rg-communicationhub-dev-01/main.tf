7f8e710c5884de361330366720e3eebf92c6cc95
Yuahen Zdanovich <yauhen.zdanovich@nsure.com>
2023-08-25 15:24:47 +0200

    add name to app service

--- Infrastructure/rg-communicationhub-dev-01/main.tf
+++ Infrastructure/rg-communicationhub-dev-01/main.tf
@@ -39,6 +39,7 @@ module "app_service" {
   source = "../../Modules/web-application"
 
   resource_group_name = "rg-communicationhub-dev-01"
+  name                = "app-commhub-mainapi-dev-nsure-01"
   location            = "eastus"
   service_plan_id     = module.service_plan.service_plan_id
 }
