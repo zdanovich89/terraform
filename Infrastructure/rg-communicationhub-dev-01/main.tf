89627df628e32fb5def287292b6a1f7c921657d9
Yuahen Zdanovich <yauhen.zdanovich@nsure.com>
2023-08-25 14:18:58 +0200

    add terraform build pipeline

--- Infrastructure/rg-communicationhub-dev-01/main.tf
+++ Infrastructure/rg-communicationhub-dev-01/main.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-sharedresources-dev-01"
+    storage_account_name = "stsharedresdevnsure01"
+    container_name       = "terraformstate"
+    key                  = "rg-communicationhub-dev-01.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
