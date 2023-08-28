b8440ea11020a45aa87918a9ef3598f1419b8a28
Yuahen Zdanovich <yauhen.zdanovich@nsure.com>
2023-08-28 14:29:25 +0200

    fix app insights main.tf

--- Modules/application_insights/main.tf
+++ Modules/application_insights/main.tf
@@ -1,4 +1,4 @@
-module "azurerm_application_insights" "app_insights" {
+resource "azurerm_application_insights" "app_insights" {
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
