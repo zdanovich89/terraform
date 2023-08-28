f99b64e0b9d97a873696769cd7a193ab897fd198
Yuahen Zdanovich <yauhen.zdanovich@nsure.com>
2023-08-28 14:34:15 +0200

    add web application type to app insights

--- Infrastructure/rg-communicationhub-dev-01/application_insights.tf
+++ Infrastructure/rg-communicationhub-dev-01/application_insights.tf
@@ -3,4 +3,5 @@ module "application_insights" {
 
   name                = "appi-commhub-dev-nsure-01"
   resource_group_name = "rg-communicationhub-dev-01"
+  application_type    = "ASP.NET web application"
 }
