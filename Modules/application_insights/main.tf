039503802be8283dd2b16fb0f9d4d5f73a81631f
Yuahen Zdanovich <yauhen.zdanovich@nsure.com>
2023-08-28 14:27:10 +0200

    edit application insights main.tf

--- Modules/application_insights/main.tf
+++ Modules/application_insights/main.tf
@@ -1,4 +1,4 @@
-data "azurerm_application_insights" "app_insights" {
+module "azurerm_application_insights" "app_insights" {
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
