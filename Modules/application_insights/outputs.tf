af37927a0c9b9cbcc9dba304fd25e9fcf9a09072
Yuahen Zdanovich <yauhen.zdanovich@nsure.com>
2023-08-28 15:31:58 +0200

    fix

--- Modules/application_insights/outputs.tf
+++ Modules/application_insights/outputs.tf
@@ -1,7 +1,7 @@
 output "application_insights_connection_string" {
-  value = module.application_insights.app_insights.connection_string
+  value = module.azurerm_application_insights.app_insights.connection_string
 }
 
 output "application_insights_instrumentation_key" {
-  value = module.application_insights.app_insights.instrumentation_key
+  value = module.azurerm_application_insights.app_insights.instrumentation_key
 }
