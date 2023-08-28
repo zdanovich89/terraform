591bd3626a06a0f73cb554c3f16a20426ccfad29
Yuahen Zdanovich <yauhen.zdanovich@nsure.com>
2023-08-28 15:19:28 +0200

    fix path to output

--- Modules/application_insights/outputs.tf
+++ Modules/application_insights/outputs.tf
@@ -1,7 +1,7 @@
 output "application_insights_connection_string" {
-  value = application_insights.connection_string
+  value = module.application_insights.application_insights_connection_string
 }
 
 output "application_insights_instrumentation_key" {
-  value = application_insights.instrumentation_key
+  value = module.application_insights.application_insights_instrumentation_key
 }
