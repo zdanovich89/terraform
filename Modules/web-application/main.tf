7cd6c00baf3054bbeee209d0c2ecf53787259b15
Yuahen Zdanovich <yauhen.zdanovich@nsure.com>
2023-08-29 11:22:44 +0200

    fix

--- Modules/web-application/main.tf
+++ Modules/web-application/main.tf
@@ -4,9 +4,13 @@ resource "azurerm_windows_web_app" "windows_app" {
   location            = var.location
   service_plan_id     = var.service_plan_id
 
-  # site_config {
-  #   always_on = true
-  # }
+  site_config {
+    always_on = true
+  }
 
   # app_settings = var.app_settings
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY"        = module.application_insights.application_insights_instrumentation_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = module.application_insights.application_insights_connection_string
+  }
 }
