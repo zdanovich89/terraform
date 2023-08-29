35c34b3445e35ea31ba95121de83cec80238476d
Yuahen Zdanovich <yauhen.zdanovich@nsure.com>
2023-08-29 12:12:39 +0200

    fix

--- Modules/web-application/main.tf
+++ Modules/web-application/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_windows_web_app" "windows_app" {
   resource_group_name = var.resource_group_name
   location            = var.location
   service_plan_id     = var.service_plan_id
-  app_settings        = var.app_settings
+  app_settings        = merge(var.app_settings)
   site_config {
     always_on = true
   }
